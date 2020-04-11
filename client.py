23ecdb6a75a1de9752c98417ec4b7e7c0a300d35
Mohnach <alt.novikov@gmail.com>
2020-04-11 19:50:50 +0300

    Получаем билеты с помощью провайдеров tutu и aviasales

--- client.py
+++ client.py
@@ -1,8 +1,51 @@
 import requests
-from time import sleep
 import json
+from TicketsFinder.aviasales import Aviasales
+from TicketsFinder.tutu import Tutu
+from TicketsFinder.tickets_finder import prepare_db_session, get_tickets
+from datetime import datetime
+from dataclasses import asdict
+from decimal import Decimal
 
 
+def find_trips(params):
+    json_params = json.loads(params)
+
+    origin_city = json_params['departure_city']
+    destination_city = 'Сочи'
+    depart_date = datetime.strptime('2020-04-21', '%Y-%m-%d')
+    return_date = datetime.strptime('2020-04-22', '%Y-%m-%d')
+
+    session = prepare_db_session()
+
+    providers_list = []
+
+    aviasales_provider = Aviasales(db_session=session)
+    providers_list.append(aviasales_provider)
+
+    tutu_provider = Tutu(db_session=session)
+    providers_list.append(tutu_provider)
+
+    print('One way tickets')
+    search_result = get_tickets(origin_city, destination_city, depart_date, providers_list)
+
+    dict_list = []
+    for item in search_result:
+        dict_list.append( asdict(item) )
+
+    result = {}
+    result['search_result'] = dict_list
+    if len(dict_list) > 0:
+        result['status'] = 'OK'
+    else:
+        result['status'] = 'Failed'
+
+#    print('Return tickets')
+#    print(get_return_tickets(origin_city, destination_city, depart_date, return_date, providers_list))
+    return result
+
+
+"""
 def find_trips(params):
     results = {
         'status': 'OK',
@@ -12,7 +55,7 @@ def find_trips(params):
         ]
     }
     return results
-
+"""
 
 def get_task():
     response = requests.get('http://127.0.0.1:8000/get_task/')
@@ -23,8 +66,15 @@ def get_task():
 def process_task(task_data):
     result = find_trips(data['task_params'])
     task_data['status'] = result['status']
-    task_data['task_result'] = json.dumps(result['search_result'])
+    task_data['task_result'] = json.dumps(result['search_result'], default=myconverter)
     return task_data
+
+
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+    if isinstance(o, Decimal):
+        return o.__str__()
 
 
 def commit_task(task_data):
