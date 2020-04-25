ea726a0095e33dd5983bd27618e577b0b8a53e08
Mohnach <alt.novikov@gmail.com>
2020-04-25 19:34:47 +0300

    Добавлена сортировка по удобности билетов

--- worker.py
+++ worker.py
@@ -3,7 +3,7 @@ import json
 from TicketsFinder.aviasales import Aviasales
 from TicketsFinder.tutu import Tutu
 from TicketsFinder.tickets_finder import prepare_db_session, get_tickets_for_all_dections, get_return_tickets_for_all_directions
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from dataclasses import asdict
 from decimal import Decimal
 from time import sleep
@@ -13,9 +13,8 @@ def find_trips(params):
     json_params = json.loads(params)
 
     origin_city = json_params['departure_city']
-    depart_date = datetime.strptime('2020-05-02', '%Y-%m-%d')
-    return_date = datetime.strptime('2020-05-03', '%Y-%m-%d')
-
+    depart_date = datetime.strptime(json_params['start_date'], '%Y-%m-%d')
+    return_date = datetime.strptime(json_params['end_date'], '%Y-%m-%d')
     session = prepare_db_session()
 
     providers_list = []
@@ -32,13 +31,12 @@ def find_trips(params):
     print('Return tickets')
     search_result += get_return_tickets_for_all_directions(origin_city, depart_date, return_date, providers_list)
 
-    dict_list = []
-    for item in search_result:
-        dict_list.append(asdict(item))
+#    list_of_tickets = convert_tickets_to_dicts(search_result)
 
-    price = lambda d: d['price'].compare(Decimal(json_params['max_price'])) == -1
-    #airline = lambda d: d['airline'] == '7R'
+    # фильтр по цене
+    price = lambda d: d.price.compare(Decimal(json_params['max_price'])) == -1
 
+    # фильтр по времени в пути
     time_conditions = []
     for time_condition in json_params['time_for_travel']:
         if time_condition == 'far':
@@ -49,30 +47,74 @@ def find_trips(params):
             time_conditions.append(timedelta(hours=3))
 
     def time_for_travel(ticket):
-        travel_time = ticket.get('travel_time')
-        if travel_time is None:
-            return True  # по-хорошему, нужно вернуть False
-        travel_time = timedelta(seconds=travel_time)
-        for time_condition in time_conditions:
-            if travel_time < time_condition:
-                return True
-        return False
+        if ticket.route_type == 'train:':
+            travel_time = ticket.travel_time  # get('travel_time')
+            travel_time = timedelta(seconds=travel_time)
+            for time_condition in time_conditions:
+                if travel_time < time_condition:
+                    return True
+            return False
+        return True  # по-хорошему, нужно вернуть False
 
+    # составляем список всех фильров
     filters = []
     filters.append(price)
     filters.append(time_for_travel)
 
-    for current_filter in filters:
-        dict_list = list(filter(current_filter, dict_list))
+    # фильтуем результат
+    search_result = filter_tickets(search_result, filters)
 
+    # сортируем по удобности
+    search_result = sort_tickets(search_result)
+
+    list_of_tickets = convert_tickets_to_dicts(search_result)
+
+    result = generate_result(list_of_tickets)
+    return result
+
+
+# попробуем вывести наверх списка удобные билеты
+def sort_tickets(tickets):
+    for ticket in tickets:
+        if ticket.arrival_time is not None:
+            if time(hour=8) < ticket.arrival_time < time(hour=13):
+                ticket.nice += 10
+            elif time(hour=13) < ticket.arrival_time < time(hour=23, minute=59):
+                ticket.nice -= 10
+            elif time(hour=0) < ticket.arrival_time < time(hour=8):
+                ticket.nice -= 10
+
+            if ticket.arrival_date.weekday() != 5:
+                ticket.nice -= 30
+
+        if time(hour=0) < ticket.depart_time < time(hour=6):
+            ticket.nice -= 10
+
+    return sorted(tickets, key=lambda x: x.nice, reverse=True)
+
+
+def convert_tickets_to_dicts(raw_tickets):
+    list_of_tickets = []
+    for item in raw_tickets:
+        list_of_tickets.append(asdict(item))
+    return list_of_tickets
+
+
+def generate_result(list_of_tickets):
     result = {}
-    result['search_result'] = dict_list
-    if len(dict_list) > 0:
+    result['search_result'] = list_of_tickets
+    if len(list_of_tickets) > 0:
         result['status'] = 'OK'
     else:
         result['status'] = 'Failed'
 
     return result
+
+
+def filter_tickets(list_of_tickets, filters):
+    for current_filter in filters:
+        list_of_tickets = list(filter(current_filter, list_of_tickets))
+    return list_of_tickets
 
 
 def get_task():
